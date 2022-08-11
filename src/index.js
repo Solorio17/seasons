6f6fd5c93e3eb8e77c7189f002454c33229f7b56
Jason <jasonsoloriom@gmail.com>
2022-08-10 21:40:12 -0600

    Using a class based component, and only returning the geolocation of the user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (error) => console.log(error)
+//   )
+
+//   return (
+//     <div>
+//       Hello Jason
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error)
+    );
+
+    return (
+      <div>
+        Hello Jason
+      </div>
+    )
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +36,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
